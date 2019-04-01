1e9a1ac1bd5575d3660f5549a4152e322eb7a701
Sanjit Thangarasu <isanjit3@gmail.com>
2019-03-31 21:24:25 -0400

    Document Formatting

--- Thangarasu_S_SierpinskyTriangle.py
+++ Thangarasu_S_SierpinskyTriangle.py
@@ -6,9 +6,10 @@ This is a program that takes two (2) user inputs, the base length of the triangl
 as well as the degree to which the program should execute the program.
 """
 
-#import turtle module and random module
-import turtle, random
-#import 'square root' function from math module
+# import turtle module and random module
+import turtle
+import random
+# import 'square root' function from math module
 from math import sqrt
 wn = turtle.Screen()
 t = turtle.Turtle()
@@ -18,6 +19,8 @@ t.hideturtle()
 The purpose of this function is mainly to set the direction and position of the turtle,
 draws the base with user inputted length
 """
+
+
 def drawLine(x, y, length, angle):
     t.up()
     """
@@ -25,7 +28,7 @@ def drawLine(x, y, length, angle):
     sets it to a specific direction on an imaginary circle with 0 = east, 90 = north, 180 = west, and 270 = south
     """
     t.setheading(angle)
-    #goes to bottom left corner
+    # goes to bottom left corner
     t.goto(x, y)
     t.down()
     t.fd(length)
@@ -34,6 +37,8 @@ def drawLine(x, y, length, angle):
 """
 this function is in charge of drawing all of the triangles inside the main triangle
 """
+
+
 def drawTriangle(x, y, length):
     t.up()
     R = random.random()
@@ -51,6 +56,8 @@ def drawTriangle(x, y, length):
 """
 This function finds the mids for the triangles
 """
+
+
 def findMid(x, y, length):
     t.up()
     t.color("White")
@@ -66,7 +73,7 @@ def sierpinski(x, y, length, degree):
     if degree <= 0:
         t.up()
         #print("Please try again with a degree greater than 0")
-        #writeText()
+        # writeText()
         return
 
     drawTriangle(x, y, length)
@@ -78,15 +85,18 @@ def sierpinski(x, y, length, degree):
     sierpinski(x + length/2, y, length/2, degree-1)
     sierpinski(x + length/4, y + (length/4)*sqrt(3), length/2, degree-1)
 
+
 def writeText():
     t.up()
     t.goto(0, -((2*length/5)*sqrt(3)))
     t.down()
     t.color("Black")
-    t.write("Sanjit Thangarasu Block A | Sierpinski Triangle to degree "+ str(degree), font=("Times New Roman", 25), align="center")
+    t.write("Sanjit Thangarasu Block A | Sierpinski Triangle to degree " +
+            str(degree), font=("Times New Roman", 25), align="center")
+
 
 def main():
-    #Gets user input for length and degree for the triangle
+    # Gets user input for length and degree for the triangle
     global length, degree
 
     length = int(input("Enter a length for the base of the Triangle: "))
@@ -94,6 +104,7 @@ def main():
 
     turtle.delay(0)
     sierpinski(-length/2, -length/2, length, degree)
+
 
 main()
 
